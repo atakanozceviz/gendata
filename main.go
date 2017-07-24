7fd635b8a10e434ed8085bd7c56cfb9348ba0251
Atakan Özceviz <atakanozceviz@hotmail.com>
2017-07-24 17:32:37 +0300

    HTML codes fixed an minified

--- main.go
+++ main.go
@@ -15,32 +15,9 @@ func main() {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		x, err := strconv.Atoi(strings.Replace(r.URL.Path, "/", "", -1))
 		if err != nil {
-			fmt.Fprintln(w, `
-			<!doctype html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport"
-					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Square Generator</title>
-			</head>
-			<body>
-			<h2>Usage:</h2>
-			<h4>Add a number to url path.</h4>
-			<h4>Example:</h4><a href="/10">`+r.Host+`/10</a>
-			</body>
-			</html>`)
+			fmt.Fprintln(w, `<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Square Generator</title></head><body><h2>Usage:</h2><h4>Add a number to url path.</h4><h4>Example:</h4><a href="/10">`+r.Host+`/10</a></body></html>`)
 		} else {
-			fmt.Fprintln(w, `
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport"
-					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Square Creator</title>
-				<style>pre { line-height:0;}</style>
-			</head>`)
+			fmt.Fprintln(w, `<!DOCTYPE html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Square Creator</title><style>pre{line-height:0;}</style></head>`)
 			for i := 1; i <= x; i++ {
 				io.WriteString(w, "<pre>")
 				for j := 1; j <= x; j++ {
@@ -59,23 +36,7 @@ func main() {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		x, err := strconv.Atoi(strings.Replace(r.URL.Path, "/data/", "", -1))
 		if err != nil {
-			fmt.Fprintln(w, `
-			<!doctype html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport"
-					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Data Generator</title>
-			</head>
-			<body>
-			<h2>Usage:</h2>
-			<h4>Add a number to url path.</h4>
-			<h4>Example:</h4><a href="/data/10">`+r.Host+`/data/10</a>
-			<h4>This will generate 10KB data</h4>
-			</body>
-			</html>`)
+			fmt.Fprintln(w, `<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Data Generator</title></head><body><h2>Usage:</h2><h4>Add a number to url path.</h4><h4>Example:</h4><a href="/data/10">`+r.Host+`/data/10</a><h4>This will generate 10KB data</h4></body></html>`)
 		} else {
 			if x < 1 {
 				io.WriteString(w, "Minimum 1KB")
